79ec03f2ee05bce3038215b174371280d68c29dc
dileep <dileep.samala@connect.qut.edu.au>
2025-04-03 21:52:07 +1000

    Update ip address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.25.139.50:5001', // live
+  baseURL: 'http://54.206.75.178:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
